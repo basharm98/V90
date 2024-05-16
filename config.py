4335024765e7f09d2fa9e5abf5cf7ef742c09c4c
basharm98 <167342964+basharm98@users.noreply.github.com>
2024-05-16 16:19:16 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
+OWNER_ID = os.environ.get("OWNER_ID", "7069511884"))
+
+
+    
